0d2881059ca04293ef537b4da187c8b2903c6165
Nissekissen <nils.m.lindblad@gmail.com>
2022-11-01 22:35:12 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js')
+const fs = require('fs')
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages]
+});
+
+client.commands = new Collection();
+
+const loadCommands = () => {
+    const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        client.commands.set(command.data.name, command);
+    }
+}
+
+const loadEvents = () => {
+    const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'))
+
+    for (const file of eventFiles) {
+        const event = require(`./events/${file}`);
+        if (event.once) {
+            client.once(event.name, (...args) => event.execute(...args));
+        } else {
+            client.on(event.name, (...args) => event.execute(...args));
+        }
+    }
+}
+
+loadCommands();
+loadEvents();
+
+client.login(token);
+
